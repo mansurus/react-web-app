71652975213f269244a986940092b22dd134deb3
Ali Mansur <amansur@mngkargo.com.tr>
2021-11-15 16:32:08 +0300

    new react app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import ExpenseItem  from "./components/ExpenseItem";
+
+function App() {
+
+   const expenses = [
+     {id:'d1', title:'5 Kg Dumbbell', amount:100,date:new Date(2021,11,15)},
+     {id:'d2', title:'10 Kg Dumbbell', amount:200,date:new Date(2021,11,16)},
+     {id:'d3', title:'15 Kg Dumbbell', amount:300,date:new Date(2021,11,17)},
+     {id:'d4', title:'25 Kg Dumbbell', amount:400,date:new Date(2021,11,18)}
+   ];
+
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
